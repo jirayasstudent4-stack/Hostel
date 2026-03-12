de967433629ac0acc96654538122a5ed955ed527
jayasuryaNew <https://github.com/jayasuryaNew/Netflix.git>
2026-03-12 12:07:52 +0530

    hostel project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Hostel/'   // MUST match repo name exactly (case-sensitive)
 })
